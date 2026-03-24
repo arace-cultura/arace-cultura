bd69170d093e5a862b340293357590ac17b78a15
pedro-ms-br <pt971121@gmail.com>
2026-03-23 23:09:03 -0300

    Initial Commit
    apenas mudanca em comentarios

--- app/src/main/java/com/aracecultura/arace/ui/Favoritos.kt
+++ app/src/main/java/com/aracecultura/arace/ui/Favoritos.kt
@@ -16,18 +16,11 @@ class Favoritos : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // 1. Desloga do Firebase
+
         FirebaseAuth.getInstance().signOut()
-
-        // 2. Redireciona para a tela de entrada (LoginActivity ou similar)
-        // Usamos requireActivity() para pegar o contexto da MainActivity
         val intent = Intent(requireActivity(), LoginActivity::class.java)
-
-        // Limpa a pilha de atividades para o usuário não conseguir voltar
         intent.flags= Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-
         startActivity(intent)
-
         requireActivity().finish()
 
         return inflater.inflate(R.layout.fragment_favoritos, container, false)
