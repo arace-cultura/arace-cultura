fdd37de4de14d03d94966d991706c044c300bcb4
Erickuri-sudo <erick12.santos60@gmail.com>
2026-06-01 20:16:18 -0300

    fix-colocacao-dos-novos-caminhos-dos-arquivos-de-user

--- app/Views/user/arace-chat.php
+++ app/Views/user/arace-chat.php
@@ -8,7 +8,7 @@
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <link href="https://fonts.googleapis.com/css2?family=Google+Sans+Flex&family=Playfair+Display:wght@700&display=swap" rel="stylesheet" />
   <script src="https://unpkg.com/lucide@latest"></script>
-  <link href="../assets/style/chat.css" rel="stylesheet" />
+  <link href="/css/chat.css" rel="stylesheet" />
 </head>
 <body>
 
