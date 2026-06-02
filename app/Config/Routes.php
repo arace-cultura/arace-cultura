ca5e77a1d9750f0b37f0f293ea6f553dd081c04e
Erickuri-sudo <erick12.santos60@gmail.com>
2026-06-02 10:08:58 -0300

    feat-ligacao-das-telas-landing-e-index-entre-si

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -3,16 +3,13 @@
 use CodeIgniter\Router\RouteCollection;
 
 /** @var RouteCollection $routes */
-$routes->view("/", "main/index");
-$routes->view("arace-landing.php", "main/arace-landing");
 
-$routes->view('entrada', 'entrada');
-$routes->view("login", "authentication/login-arace");
-$routes->view("cadastro", "authentication/cadastro-arace");
+// Rota Raiz (Ex: aponta para a landing page dentro da pasta main)
+$routes->view('/', 'main/arace-landing', ['as' => 'landing-page']);
 
-
-$routes->view("produtos", "main/produtos");
-
-$routes->view("perfil_usuario", "user/perfil");
-
-$routes->view("carrinho", "main/carrinho");
+// Demais rotas do seu projeto com seus respectivos apelidos
+$routes->view('entrada', 'entrada', ['as' => 'rota_entrada']);
+$routes->view("login", "authentication/login-arace", ['as' => 'rota_login']);
+$routes->view("cadastro", "authentication/cadastro-arace", ['as' => 'rota_cadastro']);
+$routes->view("produtos", "main/produtos", ['as' => 'rota_produtos']);
+$routes->view("carrinho", "main/carrinho", ['as' => 'rota_carrinho']);
