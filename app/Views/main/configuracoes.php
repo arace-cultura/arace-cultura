1bcc602a33901114808c9e9b22386bb01bec1b6e
Erickuri-sudo <erick12.santos60@gmail.com>
2026-06-02 11:13:24 -0300

    fix-acerto-do-nome-da-fun-route

--- app/Views/main/configuracoes.php
+++ app/Views/main/configuracoes.php
@@ -10,6 +10,6 @@
     // O PHP gera a URL e o JavaScript executa o redirecionamento
     window.location.replace("<?= url_to('main_arace_configuracao') ?>");
 </script>
-  <a href="{{routes('main_arace_configuracao')}}">Ir para Configuracoes</a>
+  <a href="{{route('main_arace_configuracao')}}">Ir para Configuracoes</a>
 </body>
 </html>
