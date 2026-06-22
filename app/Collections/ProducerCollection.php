d8bd4a27d73684b0d5ef5e2fdc9fcfedffe6627a
Erickuri-sudo <erick12.santos60@gmail.com>
2026-06-22 18:58:11 -0300

    fix-colcoacao-do-tipo-correto-do-tipo

--- app/Collections/ProducerCollection.php
+++ app/Collections/ProducerCollection.php
@@ -10,7 +10,7 @@ final class ProducerCollection extends Collection
     public const NAME   = 'produtores';
     public const ENTITY = Producer::class;
 
-    protected $validationRules = [
+    protected array $validationRules = [
         'nome'     => 'required|min_length[2]|max_length[120]',
         'email'    => 'required|valid_email',
         'telefone' => 'permit_empty|max_length[30]',
